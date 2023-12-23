da050838b63295caaed502986457fc80d6207b77
nsddevil <55065850+nsddevil@users.noreply.github.com>
2023-12-23 15:54:17 +0900

    Update getMovies.ts
    오타

--- lib/getMovies.ts
+++ lib/getMovies.ts
@@ -4,7 +4,7 @@ async function fetchFromTMDB(url: URL, cacheTime?: number) {
   url.searchParams.set('include_adult', 'false');
   url.searchParams.set('include_video', 'false');
   url.searchParams.set('sort_by', 'popularity.desc');
-  url.searchParams.set('language', 'un-US');
+  url.searchParams.set('language', 'en-US');
   url.searchParams.set('page', '1');
 
   const options: RequestInit = {
